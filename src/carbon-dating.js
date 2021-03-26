693f5745fdb4b308c1cf37a6a78423094052155c
Guzenko-Alexandr <sasha5539107@mail.ru>
2021-03-26 20:57:04 +0300

    +2

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity != "string" || typeof +sampleActivity != "number") {
+    return false
+  }
+  if (sampleActivity == undefined) {
+    return false
+  }
+  let dol = sampleActivity.trim();
+  console.log(dol)
+  if (dol / dol != 1 || dol.length == 0) {
+    return false
+  }
+  // console.log(sampleActivity.trim().length)
+  // if (sampleActivity.trim().length == 0) {
+  // return false
+  // }
+
+  if (+sampleActivity == "NaN") {
+    // console.log(NaN);
+    return false;
+  }
+
+  if (typeof sampleActivity == typeof 1 || sampleActivity <= 0 || sampleActivity > 15) {
+    // console.log(false);
+    return false;
+  }
+  // console.log(1);
+  if (typeof +sampleActivity == typeof 1 || sampleActivity != Infinity) {
+    // console.log("ok")
+    // if (sampleActivity < 0) {
+    //   let res1 = Math.log(MODERN_ACTIVITY / Math.abs(sampleActivity));
+    //   let del = Math.pow(10, -4);
+    //   let res = res1 / (0.693 / HALF_LIFE_PERIOD);
+    //   otvet = Math.ceil(res);
+    //   console.log(+`-${otvet}`)
+    //   return +`-${otvet}`
+    // } else {
+    let res1 = Math.log(MODERN_ACTIVITY / sampleActivity);
+    let del = Math.pow(10, -4);
+    let res = res1 / (0.693 / HALF_LIFE_PERIOD);
+    console.log(Math.ceil(res))
+    if (res == Infinity || res < 0) {
+      // console.log(false);
+      return false;
+    }
+    return Math.ceil(res)
+    // }
+  }
+  else { return false }
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let myArr = [];
+  // console.log(myArr.length)
+  // console.log(null.length)
+  if (Array.isArray(members) != true) {
+    console.log("nearr")
+    return false;
+  } else {
+    console.log("arr")
+  }
+
+  members.forEach(element => {
+    if (typeof element != "string") {
+      return false;
+    }
+    else {
+      myArr.push(element)
+    }
+  });
+  let result = "";
+  if (myArr.length == 0) {
+    console.log(1)
+    return false;
+  }
+  else {
+    myArr.sort();
+    myArr.forEach(element => {
+      for (let i = 0; i < element.length; i++) {
+        if (element[i] == " ") {
+          continue;
+          // result += element;
+        }
+        else {
+          result += element[i].toUpperCase()
+          break;
+        }
+      }
+    });
+  };
+  // result.sort()
+  // console.log
+  let myArr2 = result.split("").sort().join("")
+  console.log(myArr2)
+  return myArr2
+}
