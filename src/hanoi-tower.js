de98c64ee9552853a1efbe89cc1282402de2d7cc
Guzenko-Alexandr <sasha5539107@mail.ru>
2021-03-27 17:48:01 +0300

    +1

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+
+  let n = Math.pow(2, disksNumber) - 1;
+  let skorVsek = turnsSpeed * (1 / 3600);
+  // 0.0002777777777778;
+  let kolvoSekOtvet = Math.floor(n / skorVsek);
+  // console.log(n, kolvoSekOtvet)
+  let obj = {};
+  obj.turns = n;
+  obj.seconds = kolvoSekOtvet;
+  return (obj)
 };
+
